8593275ac7e6049637f2f4766c17343daa6e92f0
JohnKenneth <fetizanankennetho@gmail.com>
2024-04-24 00:04:14 +0800

    commit

--- functions/api.js
+++ functions/api.js
@@ -3,11 +3,8 @@ const serverless = require('serverless-http');
 const router = require('./routes/author');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
-// Import your Mongoose models and router
 const app = express();
-const dbLocalUrl = 'mongodb://localhost:27017/express-mongo-api';
-const dbCloudUrl = 'mongodb+srv://fetizananjohnkenneth:<password>@cluster0.dmpyias.mongodb.net/';
+const dbLocalUrl = 'mongodb://localhost:27017/sereverless';
 
 app.use(cors());
 app.use(express.json());
@@ -15,12 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
-  .connect(process.env.dbCloudUrl || dbLocalUrl, {})
+  .connect(process.env.MONGODB_URI || dbLocalUrl, {})
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Failed to connect to MongoDB', error));
 
-
-// Attempt MongoDB connection
+// app.use('/.netlify/functions/api', router);
 app.use('/api', router);
 
 const port = process.env.PORT || 5000;
